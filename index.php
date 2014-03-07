6c8fefa7408dca9f98ddad89035ed3b3a83d0541
pietrodn <powerpdn@gmail.com>
2014-03-07 16:49:25 +0100

    Adding JavaScript for dynamic menu (mobile)

--- index.php
+++ index.php
@@ -231,5 +231,11 @@
 			</p>
 		</div>
 	</div>
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//tools.wmflabs.org/static/res/jquery/2.1.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="//tools.wmflabs.org/static/res/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    
 	</body>
 </html>
