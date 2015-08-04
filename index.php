77a2e33810eaa29e5894509b438cd0091980f00a
pietrodn <powerpdn@gmail.com>
2015-08-04 16:44:14 +0200

    Updated Bootstrap and jQuery libraries

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 		<link rel="shortcut icon" href="/favicon.ico" />
 
 		<title>Intersect Contribs - Wikimedia Tool Labs</title>
-		<link href="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+		<link href="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
         <link href="pietrodn.css" rel="stylesheet">
 	</head>
 <body>
@@ -239,9 +239,9 @@
 	</div>
 	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+    <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
     
 	</body>
 </html>
